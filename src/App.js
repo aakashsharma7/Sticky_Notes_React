745c6e71a15cb6cfc5ca656d827041b660c03f10
aakashsharma7 <aakashsharma.ghd@gmail.com>
2024-07-29 17:49:14 +0530

    javascript

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import EmptyNotes from "./components/EmptyNotes";
+import Header from "./components/Header";
+import StickyNotesGrid from "./components/StickyNotesGrid";
+import StickyNotesProvider from "./context/StickyNotesContext";
+import "./index.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StickyNotesProvider>
+      <div className="app-container">
+        <Header />
+        <StickyNotesGrid />
+        <EmptyNotes />
+      </div>
+    </StickyNotesProvider>
   );
 }
 
